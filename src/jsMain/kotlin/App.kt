68cb57bba726c5600c25d8413efe737b999e1908
EmileLevast <emilelevast@outlook.fr>
2023-04-08 22:55:44 +0200

    [FEAT] reuse page to search armes

--- src/jsMain/kotlin/App.kt
+++ src/jsMain/kotlin/App.kt
@@ -9,6 +9,7 @@ import mui.material.Box
 import mui.material.Grid
 import mui.material.Tab
 import mui.material.Tabs
+import react.dom.html.ReactHTML
 import react.dom.html.ReactHTML.button
 import react.dom.html.ReactHTML.h1
 
@@ -27,36 +28,68 @@ val App = FC<Props> {
             //bddList = getBddList()
         }
     }
-Box{
-    TabContext{
-        value = activeTab
-        Box{
-            Tabs{
-                onChange = { _, newValue -> activeTab = newValue }
-                ariaLabel = "wrapped label tabs example"
+    Box {
+        TabContext {
+            value = activeTab
+            Box {
+                Tabs {
+                    onChange = { _, newValue -> activeTab = newValue }
+                    ariaLabel = "wrapped label tabs example"
 
 
 
-                Tab{
-                    label = ReactNode("1")
-                    value = "1"
-                }
-                Tab{
-                    label = ReactNode("2")
-                    value = "2"
+                    Tab {
+                        label = ReactNode("1")
+                        value = "1"
+                    }
+                    Tab {
+                        label = ReactNode("2")
+                        value = "2"
+                    }
                 }
             }
-        }
-        TabPanel{
-            value = "1"
-            +"Bonjour"
-        }
-        TabPanel{
-            value = "2"
-            +"Aurevoir"
+            TabPanel {
+                value = "1"
+                h1 {
+                    +"Dark Soul List"
+                }
+                inputComponent {
+                    onSubmit = { input ->
+                        //Ici on va chercher les items dans la bdd
+                        scope.launch {
+                            val itemsFound: List<IListItem> = searchAnything(input.cleanupForDB())
+                            logger.debug("Cherche un element $input : ${itemsFound.joinToString { it.nom }}")
+                            if (itemsFound.isNotEmpty())
+                            {
+                                mutableListOf<IListItem>().let{
+                                    it.addAll(bddList)
+                                    it.addAll(itemsFound)
+                                    bddList = it
+                                }
+                            }
+                        }
+                    }
+                }
+                button{
+                    onClick = {
+                        bddList = bddList.filter{it.isAttached}
+                    }
+                    + "Clear"
+                }
+                Grid {
+                    bddList.forEach {
+                        itemListComponent{
+                            itemList = it
+                        }
+                    }
+                }
+            }
+            TabPanel {
+                value = "2"
+                +"Aurevoir"
+            }
         }
     }
-}
 //    <Box sx={{ width: "100%", typography: "body1" }}>
 //    <TabContext value={value}>
 //    <Box sx={{ borderBottom: 1,
@@ -75,8 +108,6 @@ Box{
 //    <TabPanel value="3">Web Development</TabPanel>
 //    </TabContext>
 //    </Box>
-
-
 
 
 }
