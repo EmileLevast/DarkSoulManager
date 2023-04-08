83b52413237af8ee457276c007c97e5b3032885d
EmileLevast <emilelevast@outlook.fr>
2023-04-08 22:38:01 +0200

    [FEAt] change main screen by tab with useless content

--- src/jsMain/kotlin/App.kt
+++ src/jsMain/kotlin/App.kt
@@ -1,7 +1,11 @@
 import io.ktor.util.logging.*
 import react.*
 import kotlinx.coroutines.*
+import mui.lab.TabContext
+import mui.lab.TabList
 import mui.lab.TabPanel
+import mui.material.Box
+
 import mui.material.Grid
 import mui.material.Tab
 import mui.material.Tabs
@@ -13,9 +17,9 @@ val logger = KtorSimpleLogger("logger")
 
 val App = FC<Props> {
 
-    var bddList:List<IListItem> by useState(emptyList<IListItem>())
+    var bddList: List<IListItem> by useState(emptyList<IListItem>())
 
-    var activeTab by useState("Recherche")
+    var activeTab by useState("1")
 
     useEffectOnce {
         scope.launch {
@@ -23,55 +27,47 @@ val App = FC<Props> {
             //bddList = getBddList()
         }
     }
-
-    Tabs{
+Box{
+    TabContext{
         value = activeTab
-        onChange = { _, newValue -> activeTab = newValue }
-        ariaLabel = "wrapped label tabs example"
-        Tab{
-            value="Recherche"
+        Box{
+            TabList{
+                Tab{
+                    label = ReactNode("1")
+                    value = "1"
+                }
+                Tab{
+                    label = ReactNode("2")
+                    value = "2"
+                }
+            }
+        }
+        TabPanel{
+            value = "1"
+            +"Bonjour"
+        }
+    }
+}
+//    <Box sx={{ width: "100%", typography: "body1" }}>
+//    <TabContext value={value}>
+//    <Box sx={{ borderBottom: 1,
+//    borderColor: "divider" }}>
+//    <TabList
+//    onChange={handleChange}
+//    aria-label="lab API tabs example"
+//    >
+//    <Tab label="Tutorial 1" value="1" />
+//    <Tab label="Tutorial 2" value="2" />
+//    <Tab label="Tutorial 3" value="3" />
+//    </TabList>
+//    </Box>
+//    <TabPanel value="1">Data Structures</TabPanel>
+//    <TabPanel value="2">Algorithms</TabPanel>
+//    <TabPanel value="3">Web Development</TabPanel>
+//    </TabContext>
+//    </Box>
 
-        }
-        Tab{
-            value="Modification"
-        }
-    }
-    TabPanel{
-        value="Recherche"
-//        tabIndex=0
-//        h1 {
-//            +"Dark Soul List"
-//        }
-//        inputComponent {
-//            onSubmit = { input ->
-//                //Ici on va chercher les items dans la bdd
-//                scope.launch {
-//                    val itemsFound: List<IListItem> = searchAnything(input.cleanupForDB())
-//                    logger.debug("Cherche un element $input : ${itemsFound.joinToString { it.nom }}")
-//                    if (itemsFound.isNotEmpty())
-//                    {
-//                        mutableListOf<IListItem>().let{
-//                            it.addAll(bddList)
-//                            it.addAll(itemsFound)
-//                            bddList = it
-//                        }
-//                    }
-//                }
-//            }
-//        }
-//        button{
-//            onClick = {
-//                bddList = bddList.filter{it.isAttached}
-//            }
-//            + "Clear"
-//        }
-//        Grid {
-//            bddList.forEach {
-//                itemListComponent{
-//                    itemList = it
-//                }
-//            }
-//        }
-    }
+
+
 
 }
