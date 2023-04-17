1cfa8fffcd24154e64f6383f45d8eb8191ee7cce
EmileLevast <emilelevast@outlook.fr>
2023-04-17 13:29:59 +0200

    [FEAT] Read from all textFields

--- src/jsMain/kotlin/TabTextFieldsComponent.kt
+++ src/jsMain/kotlin/TabTextFieldsComponent.kt
@@ -1,9 +1,17 @@
 import mui.material.Box
+import mui.material.LabelPlacement
 import mui.material.Stack
 import mui.material.TextField
+import org.w3c.dom.HTMLDivElement
+import org.w3c.dom.HTMLInputElement
 import react.FC
 import react.Props
 import react.ReactNode
+import react.dom.events.ChangeEventHandler
+import react.dom.events.FormEventHandler
+import react.dom.html.ReactHTML.button
+import react.dom.html.ReactHTML.h4
+import react.dom.onChange
 import react.useState
 
 external interface TabTextFieldProps : Props {
@@ -11,13 +19,34 @@ external interface TabTextFieldProps : Props {
 }
 
 val tabTextFieldComponent = FC<TabTextFieldProps> { props ->
-    var listParcelableAttribute:List<String>
+    var listModifiedAttribute = mutableListOf<String>()
+
+
+
 
     Stack{
         props.itemList.getParsingRulesAttributesAsList().forEach {
+
+            val (text, setText) = useState("")
+            val changeHandler: FormEventHandler<HTMLDivElement> = {
+                setText(((it.target) as HTMLInputElement).value)
+            }
+
             TextField{
                 helperText = ReactNode(it)
+                onChange = changeHandler
             }
+
+            listModifiedAttribute.add(text)
+        }
+
+        button{
+            onClick = {
+                listModifiedAttribute.forEach {
+                    logger.debug(">$it")
+                }
+            }
+            + "Lecture"
         }
     }
 }
