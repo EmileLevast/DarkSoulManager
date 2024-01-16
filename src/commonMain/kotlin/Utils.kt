e7495cb11c6e6d0623b795eab5e2e22139d78cb9
emile <emilelevast@outlook.fr>
2024-01-16 16:18:12 +0100

    [FIX] Les apostrophes dans les noms

--- src/commonMain/kotlin/Utils.kt
+++ src/commonMain/kotlin/Utils.kt
@@ -1,3 +1,3 @@
 fun String.cleanupForDB():String{
-    return this.lowercase().replace("'"," ").replace("é","e").replace("è","e").replace("î","i").replace("ï","i").replace("ä","a")
+    return this.lowercase().replace("'"," ").replace("é","e").replace("è","e").replace("î","i").replace("ï","i").replace("ä","a").replace("'"," ")
 }
